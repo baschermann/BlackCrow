b113beab69961e46e1af30c8900b8736d8ee6ea9
Florian Klemme <mail@florianklemme.de>
2017-03-20 19:05:22 +0100

    Minor changes.

--- src/Map.cpp
+++ src/Map.cpp
@@ -5,23 +5,19 @@ namespace BlackCrow {
 	using namespace BWAPI;
 	using namespace Filter;
 
-	Map::Map(BlackCrow &parent) : bc(parent) {}
-
-	Map::~Map() {}
-
-	void Map::onStart() {
+	Map::Map(BlackCrow &parent) : bc(parent) {
 		// Map Tiles
 		tileWidth = Broodwar->mapWidth();
 		tileHeight = Broodwar->mapHeight();
 
-		mapTiles = std::vector<std::vector<Cell>>(tileWidth);
-		for (int i = 0; i < tileWidth; i++) {
-			mapTiles[i] = std::vector<Cell>(tileHeight);
-		}
+		mapTiles = std::vector<std::vector<Cell>>(tileWidth, std::vector<Cell>(tileHeight));
+	}
 
+	Map::~Map() {}
+
+	void Map::onStart() {
 		for (int x = 0; x < tileWidth; x++) {
 			for (int y = 0; y < tileHeight; y++) {
-				mapTiles[x][y] = Cell();
 				mapTiles[x][y].buildable = Broodwar->isBuildable(x, y, true);
 				mapTiles[x][y].resourceBuildable = true;
 				mapTiles[x][y].mineralLine = true;
