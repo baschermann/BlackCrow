9ee92b403662873293511f87900d4d077b14eafe
Dukrah <durrean@anti-cheat.com>
2017-06-28 23:12:38 +0200

    Changed code generation from /MD to /MT

--- build.bat
+++ build.bat
@@ -3,6 +3,9 @@ pushd %CD%
 cd bwapi\bwapi
 msbuild /p:Configuration=Debug   /p:Platform=Win32 /target:BWAPI bwapi.sln
 msbuild /p:Configuration=Release /p:Platform=Win32 /target:BWAPI bwapi.sln
+
+msbuild /p:Configuration=Debug   /p:Platform=Win32 /target:BWAPIClient bwapi.sln
+msbuild /p:Configuration=Release /p:Platform=Win32 /target:BWAPIClient bwapi.sln
 popd
 
 :: Build KBot
--- src/BlackCrow.h
+++ src/BlackCrow.h
@@ -18,26 +18,26 @@
 // Remember not to use "Broodwar" in any global class constructor!
 namespace BlackCrow {
 	
-	class BlackCrow : public BWAPI::AIModule {
+	class BlackCrow {
 	public:
 		// Virtual functions for callbacks, leave these as they are.
-		virtual void onStart();
-		virtual void onEnd(bool isWinner);
-		virtual void onFrame();
-		virtual void onSendText(std::string text);
-		virtual void onReceiveText(BWAPI::Player player, std::string text);
-		virtual void onPlayerLeft(BWAPI::Player player);
-		virtual void onNukeDetect(BWAPI::Position target);
-		virtual void onUnitDiscover(BWAPI::Unit unit);
-		virtual void onUnitEvade(BWAPI::Unit unit);
-		virtual void onUnitShow(BWAPI::Unit unit);
-		virtual void onUnitHide(BWAPI::Unit unit);
-		virtual void onUnitCreate(BWAPI::Unit unit);
-		virtual void onUnitDestroy(BWAPI::Unit unit);
-		virtual void onUnitMorph(BWAPI::Unit unit);
-		virtual void onUnitRenegade(BWAPI::Unit unit);
-		virtual void onSaveGame(std::string gameName);
-		virtual void onUnitComplete(BWAPI::Unit unit);
+		void onStart();
+		void onEnd(bool isWinner);
+		void onFrame();
+		void onSendText(std::string text);
+		void onReceiveText(BWAPI::Player player, std::string text);
+		void onPlayerLeft(BWAPI::Player player);
+		void onNukeDetect(BWAPI::Position target);
+		void onUnitDiscover(BWAPI::Unit unit);
+		void onUnitEvade(BWAPI::Unit unit);
+		void onUnitShow(BWAPI::Unit unit);
+		void onUnitHide(BWAPI::Unit unit);
+		void onUnitCreate(BWAPI::Unit unit);
+		void onUnitDestroy(BWAPI::Unit unit);
+		void onUnitMorph(BWAPI::Unit unit);
+		void onUnitRenegade(BWAPI::Unit unit);
+		void onSaveGame(std::string gameName);
+		void onUnitComplete(BWAPI::Unit unit);
 		// Everything below this line is safe to modify.
 
 		BWEM::Map &bwem;
