710c265a765703aac2bdb68b320c03f12bd11c90
Dukrah <durrean@anti-cheat.com>
2017-06-29 01:11:58 +0200

    Made project working again

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 :: Build BWAPI
 pushd %CD%
 cd bwapi\bwapi
-msbuild /p:Configuration=Debug   /p:Platform=Win32 /target:BWAPI bwapi.sln
-msbuild /p:Configuration=Release /p:Platform=Win32 /target:BWAPI bwapi.sln
+msbuild /p:Configuration=Debug   /p:Platform=Win32 /target:BWAPILIB bwapi.sln
+msbuild /p:Configuration=Release /p:Platform=Win32 /target:BWAPILIB bwapi.sln
 
 msbuild /p:Configuration=Debug   /p:Platform=Win32 /target:BWAPIClient bwapi.sln
 msbuild /p:Configuration=Release /p:Platform=Win32 /target:BWAPIClient bwapi.sln
